043c833cb80496f82d4a21aeecc504f383db6d0d
Samin Khan <saminkhann1@gmail.com>
2019-11-29 17:13:18 -0500

    chage folder structure

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// // const MongoClient = require('mongodb').MongoClient;
-// const mongoose = require('mongoose');
-
-
-// const uri = "mongodb+srv://csc309Phil:csc309csc309@cluster0-1bsyp.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect()
-
-// // mongoose.Promise = global.Promise;
-// // mongoose.connect('mongodb+srv://csc309Phil:csc309csc309@cluster0-1bsyp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
-
-
-// // Create a new Express application.
-// const app = express();
-// require('./routes/questionRoutes')(app);
-
-
-
-
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT);
-
 const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
@@ -33,9 +10,9 @@ var app = Express();
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
-var database
+var database;
 
-app.listen(8000, () => {
+app.listen('/', (req, res) => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
         if(error) {
             throw error;
@@ -54,3 +31,7 @@ app.get("/questions", (request, response) => {
         response.send(result);
     });
 });
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT)
