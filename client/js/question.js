a330a1445d965c2c13c660da8da45c0458ca3277
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2019-12-03 16:32:20 -0500

    Fixed sidebar output

--- client/js/question.js
+++ client/js/question.js
@@ -1,9 +1,31 @@
 let questionAnswer = ""
 let myAssignments = []
+let currentAid = ""
+let currentAssignment = {}
+questionTexts = []
 
 // ID of currently displayed question
 currentQuestion = window.location.href.split('/').pop()
-console.log(currentQuestion)
+currentAid = window.location.href.split('/')[window.location.href.split('/').length - 2]
+
+console.log("current assignment: " + currentAid)
+console.log("current question: " + currentQuestion)
+
+// get current assignment
+
+function getAssignment() {
+  const url = "/api/ass/" + currentAid
+  console.log(url)
+  fetch(url).then((res) => {
+    if (res.status === 200) {
+      return res.json()
+    } else {
+      alert('Could not load assignment')
+    }
+  }).then((json) => {
+    displayAssignment(json)
+  })
+}
 
 // get DOM elements
 const assignmentTitle = document.querySelector('#assignmentTitle');
@@ -21,22 +43,9 @@ const questionPromptDisplay = document.querySelector('#questionPrompt');
 
 const feedbackText = document.querySelector('#feedback');
 
-class Assignment {
-  constructor(title, dueDateTime, questions){
-    this.title = title;
-    this.dueDateTime = dueDateTime;
-    this.questions = questions;
-    this.completed = 0;
-    this.total = questions.length;
-    this.assignmentId = numberOfAssignments;
-    numberOfAssignments++;
-  }
-}
-
 // get question text and answer, should get from server
 function getQuestion() {
   const url = '/api/questions/' + currentQuestion
-  console.log(url)
 
   fetch(url).then((res) => {
     if (res.status === 200) {
@@ -45,7 +54,6 @@ function getQuestion() {
       alert('Could not get question')
     }
   }).then((json) => {
-    console.log(json)
     // questionDisplay.innerText =
     questionTitleDisplay.innerText = json.question;
     questionCategoryDisplay.innerText = json.qid;
@@ -71,26 +79,39 @@ function getAssignments() {
     }
   }).then((json) => {
     myAssignments = json
-    console.log(json)
-    if (myAssignments.length == 0){
+    if (myAssignments.length == 0 || currentAid == "practice"){
       assignmentSidebar.style.display = "none";
     } else {
-      // displayAssignment(myAssignments[0].aid)
-      // TODO replace 0 with link aid from url
-      displayAssignment(0);
+      getAssignment()
     }
   })
 }
 
-function displayAssignment(aid){
-  assignmentTitle.innerText = myAssignments[aid].name;
-  // assignmentDue.innerText = myAssignments[aid].dueDateTime;
+function getQuestionPreview(qid, newPreview, i) {
+  const url = '/api/questions/' + qid
+
+  fetch(url).then((res) => {
+    if (res.status === 200) {
+      return res.json()
+    } else {
+      alert('Could not get questions')
+    }
+  }).then((json) => {
+    console.log(json.question)
+    questionTexts.push(json.question)
+    newPreview.innerText = questionTexts[i]
+  })
+}
+
+
+function displayAssignment(assignment){
+  assignmentTitle.innerText = assignment.name;
+   assignmentDue.innerText = assignment.due;
   // assignmentCompletion.innerText = (myAssignments[0].completed / myAssignments[0].total).toFixed(2) * 100 + '%';
   questionList.innerHTML = "";
   // for each question, add to sidebar - requires server calls
-  for (let i = 0; i < myAssignments[aid].questions.length; i++){
-    const questionUrl = '/api/questions/' + myAssignments[aid].questions[i]
-    console.log(questionUrl)
+  for (let i = 0; i < assignment.questions.length; i++){
+    const questionUrl = '/api/questions/' + assignment.questions[i]
 
     fetch(questionUrl).then((res) => {
       if (res.status === 200) {
@@ -99,7 +120,6 @@ function displayAssignment(aid){
         alert('Could not get assignment question')
       }
     }).then((json) => {
-      console.log(json)
       newQuestion = document.createElement('li');
       newQuestion.classList.add('questionListItem');
       newQuestionType = document.createElement('p');
@@ -109,7 +129,7 @@ function displayAssignment(aid){
       newPreview = document.createElement('p');
       newPreview.classList.add('questionPreviewText');
       newPreview.classList.add('sm-txt');
-      newPreview.innerText = questions[myAssignments[0].questions[i]];
+      getQuestionPreview(assignment.questions[i], newPreview, i)
       newQuestion.appendChild(newPreview);
       /*
       if (questions[myAssignments[0].questions[i]].completed){
@@ -123,7 +143,6 @@ function displayAssignment(aid){
     })
   }
 }
-
 
 function getForQuestionType(qid, q) {
   switch (qid.slice(0, 1)) {
