888a6b71f79dd551e2381aa722a9f9faa3145a36
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2019-12-03 18:07:31 -0500

    Implemented login

--- client/js/login.js
+++ client/js/login.js
@@ -4,29 +4,53 @@
 const idInput = document.querySelector('#userName');
 const pwInput = document.querySelector('#passWord');
 const loginButton = document.querySelector('#loginButton');
+let students = []
 
 // add event listener to login
 loginButton.addEventListener('click', checkCredentials);
 pwInput.addEventListener('keyup', checkEnter);
 
+// get all users
+function getStudents() {
+  const url = "/api/students"
+
+  fetch(url).then((res) => {
+    if (res.status === 200) {
+      console.log("returning")
+      return res.json()
+    } else {
+      alert('Could not load users')
+    }
+  }).then((json) => {
+    students = json
+  })
+}
+
+getStudents()
+
 // check login credentials and go to appropriate dashboard
 function checkCredentials(e) {
-  if (idInput.value == 'user' && pwInput.value == 'user'){
-    window.location.href = '/student/dashboard';
-  }
-  else if (idInput.value == 'admin' && pwInput.value == 'admin'){
-    document.location.href = '/admin/dashboard';
-  }
-  else {
-    let field = document.querySelector('#errorMessage');
-    let message = document.createElement('p');
-    if (field.innerHTML.trim().length === 0) {
-      message.innerHTML = "*username or password entered is incorrect";
-      message.style.color = "red"
+  const user = idInput.value
+  const pw = pwInput.value
+
+  for (let i = 0; i < students.length; i++) {
+    if (user == students[i].username && pw == students[i].password){
+      window.location.href = '/student/dashboard';
+      break
     }
-    field.appendChild(message)
+    else if (user== 'admin' && pw== 'admin'){
+      document.location.href = '/admin/dashboard';
+    }
   }
+  let field = document.querySelector('#errorMessage');
+  let message = document.createElement('p');
+  if (field.innerHTML.trim().length === 0) {
+    message.innerHTML = "*username or password entered is incorrect";
+    message.style.color = "red"
+  }
+  field.appendChild(message)
 }
+
 
 // if enter pressed on pwInput, click button
 function checkEnter(e) {
