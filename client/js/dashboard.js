9e3619bfd59c3d393cbb7a6a235aa7b00d7672a7
Sieun Lee <sieungrey.lee@mail.utoronto.ca>
2019-12-03 03:02:28 -0500

    Assignment cards call API

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -3,7 +3,7 @@ const API_URL = 'http://localhost:5000'
 
 // get DOM elements
 const assignmentBox = document.querySelector('#assignments');
-const myAssignments = [];
+let myAssignments = [];
 let numberOfAssignments = 0;
 
 class Assignment {
@@ -29,36 +29,35 @@ function getAssignments() {
     }
   }).then((json) => {
     console.log(json)
+    myAssignments = json
+
+    assignmentBox.innerHTML = '';
+    for (let i = 0; i < myAssignments.length; i++){
+      const newCardContainer = document.createElement('div');
+      newCardContainer.classList.add('flex-item');
+      const newLink = document.createElement('a');
+      newLink.href = "./practice.html"
+      newCardContainer.appendChild(newLink);
+      const newCard = document.createElement('div');
+      newCard.classList.add('card--h');
+      newLink.appendChild(newCard);
+      const newHeader = document.createElement('div');
+      newHeader.classList.add('card__header');
+      newCard.appendChild(newHeader);
+      const newTitle = document.createElement('h4');
+      newTitle.innerText = myAssignments[i].name;
+      newHeader.appendChild(newTitle);
+      const newDueDate = document.createElement('p');
+  //    newDueDate.innerText = "Due " + myAssignments[i].dueDateTime;
+  //    newHeader.appendChild(newDueDate);
+  //    const newCompletion = document.createElement('p');
+  //    newCompletion.innerText = myAssignments[i].completed / myAssignments[i].total + "% completed"
+  //    newHeader.appendChild(newCompletion);
+
+      assignmentBox.appendChild(newCardContainer);
+    }
   })
 }
 
-getAssignments()
-
 // get current assignments - requires server call
-myAssignments.push(new Assignment('A1', 'October 16, 2019 23:59', [1,2]));
-
-// create card for each assignment
-for (let i = 0; i < myAssignments.length; i++){
-  const newCardContainer = document.createElement('div');
-  newCardContainer.classList.add('flex-item');
-  const newLink = document.createElement('a');
-  newLink.href = "./practice.html"
-  newCardContainer.appendChild(newLink);
-  const newCard = document.createElement('div');
-  newCard.classList.add('card--h');
-  newLink.appendChild(newCard);
-  const newHeader = document.createElement('div');
-  newHeader.classList.add('card__header');
-  newCard.appendChild(newHeader);
-  const newTitle = document.createElement('h4');
-  newTitle.innerText = myAssignments[i].title;
-  newHeader.appendChild(newTitle);
-  const newDueDate = document.createElement('p');
-  newDueDate.innerText = "Due " + myAssignments[i].dueDateTime;
-  newHeader.appendChild(newDueDate);
-  const newCompletion = document.createElement('p');
-  newCompletion.innerText = myAssignments[i].completed / myAssignments[i].total + "% completed"
-  newHeader.appendChild(newCompletion);
-
-  assignmentBox.appendChild(newCardContainer);
-}
+getAssignments()
